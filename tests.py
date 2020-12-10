0d171cd7c032bfa0349f4263f2cda2d0352c482d
Vidalsagi@gmail.com <Vidalsagi@gmail.com>
2020-12-10 18:51:35 +0200

    New project start

---
+++ tests.py
@@ -0,0 +1,11 @@
+
+
+class Bool():
+    def __init__(self):
+        self.isTrue = True
+
+    def change(self):
+        if self.isTrue:
+            self.isTrue = False
+        else:
+            self.isTrue = True
